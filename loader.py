9135d0abe56dd40247948f5747985e44ac4616c1
MykhailoKlimchuk <mikeklimchuck@gmail.com>
2024-05-20 16:35:51 +0300

    commit

--- loader.py
+++ loader.py
@@ -11,61 +11,6 @@ import lox365 as lx
 class Lox365(unohelper.Base, XLox365):
     def __init__(self, ctx): self.ctx = ctx
 
-    def _get_dataarray(self, cellrange, positions: dict) -> tuple[tuple]:
-        """Return the DataArray for the given cell range object and
-        desired corner positions, which must be a dict with the keys:
-        left, top, right, bottom.
-        """
-        return cellrange.getCellRangeByPosition(
-            positions['left'], positions['top'],
-            positions['right'], positions['bottom'],).DataArray
-
-    def _get_shrunk_corners(self, cellrange) -> dict:
-        """Find the rectangular cell range that encloses all computable
-        content by removing from the given cell range all empty cells
-        from the bottom and right. Then, return its corner positions
-        as a dict with the keys: left, top, right, bottom.
-
-        Computable content here refers to cells that have:
-        numeric value, datetime, string, or formula.
-        """
-        address = cellrange.RangeAddress
-        useful_ranges = cellrange.queryContentCells(23).RangeAddresses
-        useful_positions = {'left': 0, 'top': 0,
-            'right':  max(range.EndColumn for range in useful_ranges) - address.StartColumn,
-            'bottom': max(range.EndRow    for range in useful_ranges) - address.StartRow,
-        }
-        return useful_positions
-
-    def FILTER   (self, *args): return lx.FILTER   (*args)
-    def IMAGE    (self, *args): return lx.IMAGE    (*args)
-    def SORT     (self, *args): return lx.SORT     (*args)
-    def TEXTSPLIT(self, *args): return lx.TEXTSPLIT(*args)
-
-    def TOCOL    (self, *args): return lx.TOCOL    (*args)
-
-    def UNIQUE(self, *args):
-        shrunk_corners = self._get_shrunk_corners(args[0])
-        shrunk_dataarray = self._get_dataarray(args[0], shrunk_corners)
-        args = (shrunk_dataarray,)
-        return lx.UNIQUE(*args)
-
-    def XLOOKUP(self, *args):
-        shrunk_corners1 = self._get_shrunk_corners(args[1])
-        shrunk_corners2 = self._get_shrunk_corners(args[2])
-        shrunk_corners_common_bottom = max(
-            shrunk_corners1['bottom'], shrunk_corners2['bottom'])
-        shrunk_dataarray1 = self._get_dataarray(args[1], {
-            'left': 0, 'top': 0,
-            'right': shrunk_corners1['right'],
-            'bottom': shrunk_corners_common_bottom})
-        shrunk_dataarray2 = self._get_dataarray(args[2], {
-            'left': 0, 'top': 0,
-            'right': shrunk_corners2['right'],
-            'bottom': shrunk_corners_common_bottom})
-        args = (args[0], shrunk_dataarray1, shrunk_dataarray2, *args[3:],)
-        return lx.XLOOKUP(*args)
-
     def TBACC(self, *args):
         desktop = self.ctx.ServiceManager.createInstanceWithContext(
             "com.sun.star.frame.Desktop", self.ctx)
--- pythonpath/lox365.py
+++ pythonpath/lox365.py
@@ -7,64 +7,6 @@ import urllib.request
 ERR_CALC = '#CALC!'
 ERR_NA = '#N/A'
 
-def FILTER(array, include, ifEmpty=ERR_CALC):
-    if ifEmpty is None: ifEmpty = ERR_CALC
-    lookup_direction = 0 # 0 is vertical; 1 is horizontal
-    if len(include) == 1 and len(include[0]) > 1: lookup_direction = 1
-    import itertools
-    if lookup_direction == 0:
-        ans = tuple(itertools.compress(array, [i[0] for i in include]))
-        if len(ans) == 1:
-            ans = (ans[0], tuple([0] * len(array[0])),)
-    else:
-        ans = tuple(tuple(itertools.compress(row, include[0])) for row in array)
-    return ans if ans else ((ifEmpty,),)
-
-try:
-    import imagefn
-    IMAGE = imagefn.IMAGE
-except ImportError: pass
-
-def SORT(array, sortIndex=1, sortOrder=1):
-    if sortIndex is None: sortIndex = 1
-    if sortOrder is None or sortOrder == 1: reverse = False
-    elif sortOrder == -1: reverse = True
-    else: return ValueError
-    stringify = True
-    if all(isinstance(item[sortIndex - 1], float) for item in array):
-        stringify = False
-    return tuple(sorted(array,
-        key=lambda r: str(r[sortIndex - 1]) if stringify else r[sortIndex - 1],
-        reverse=reverse))
-
-def TEXTSPLIT(text, colDelimiter):
-    if text == '' or text == '0': return ((0,),) # Compensate for when LO Calc converts a blank cell to a '0' string.
-    t2 = text.split(colDelimiter)
-    if len(t2) == 1: t2.append(0) # If output is only one item, append a zero to ensure the array function works as intended.
-    return (tuple(t2),)
-
-def TOCOL(array):
-    result = []
-    for row in array:
-        for item in row:
-            result.append((item,))
-    return tuple(result)
-
-def UNIQUE(array):
-    return tuple(dict.fromkeys(array))
-
-def XLOOKUP(lookupValue, lookupArray, returnArray, ifNotFound=ERR_NA):
-    if ifNotFound is None: ifNotFound = ERR_NA
-    lookup_direction = 0 # 0 is vertical; 1 is horizontal
-    if len(lookupArray) == 1 and len(lookupArray[0]) > 1: lookup_direction = 1
-    try:
-        if lookup_direction == 0:
-            return (returnArray[lookupArray.index((lookupValue,))],)
-        if lookup_direction == 1:
-            return tuple((row[lookupArray[0].index(lookupValue)],) for row in returnArray)
-    except ValueError: return ((ifNotFound,),)
-
-
 def format_data(func):
     def inner(*args, **kwargs):
         data = func(*args, **kwargs)
